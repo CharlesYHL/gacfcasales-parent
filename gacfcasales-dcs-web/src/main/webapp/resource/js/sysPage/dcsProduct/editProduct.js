bf1df71619b30b0e5ca83cae849234e43906ed34
yezhenqiang <zhenqiang_ye@163.com>
2018-01-26 11:09:46 +0800

    update bug

--- gacfcasales-dcs-web/src/main/webapp/resource/js/sysPage/dcsProduct/editProduct.js
+++ gacfcasales-dcs-web/src/main/webapp/resource/js/sysPage/dcsProduct/editProduct.js
@@ -9,14 +9,19 @@ $(function() {
 	if (productFait != null && productFait != '') {
 		if (productFait == '10041001') {
 			jQuery("#isFiat").append("<option value='0'>请选择</option>");
-			jQuery("#isFiat").append("<option value='10041001' selected = 'selected'>是</option>");
+			jQuery("#isFiat")
+					.append(
+							"<option value='10041001' selected = 'selected'>是</option>");
 			jQuery("#isFiat").append("<option value='10041002'>否</option>");
 		} else if (productFait == '10041002') {
 			jQuery("#isFiat").append("<option value='0'>请选择</option>");
 			jQuery("#isFiat").append("<option value='10041001'>是</option>");
-			jQuery("#isFiat").append("<option value='10041002' selected = 'selected'>否</option>");
+			jQuery("#isFiat")
+					.append(
+							"<option value='10041002' selected = 'selected'>否</option>");
 		} else {
-			jQuery("#isFiat").append("<option value='0' selected = 'selected'>请选择</option>");
+			jQuery("#isFiat").append(
+					"<option value='0' selected = 'selected'>请选择</option>");
 			jQuery("#isFiat").append("<option value='10041001'>是</option>");
 			jQuery("#isFiat").append("<option value='10041002'>否</option>");
 		}
@@ -25,14 +30,19 @@ $(function() {
 	if (productJeep != null && productJeep != '') {
 		if (productJeep == '10041001') {
 			jQuery("#isJeep").append("<option value='0'>请选择</option>");
-			jQuery("#isJeep").append("<option value='10041001' selected = 'selected'>是</option>");
+			jQuery("#isJeep")
+					.append(
+							"<option value='10041001' selected = 'selected'>是</option>");
 			jQuery("#isJeep").append("<option value='10041002'>否</option>");
 		} else if (productJeep == '10041002') {
 			jQuery("#isJeep").append("<option value='0'>请选择</option>");
 			jQuery("#isJeep").append("<option value='10041001'>是</option>");
-			jQuery("#isJeep").append("<option value='10041002'  selected = 'selected'>否</option>");
+			jQuery("#isJeep")
+					.append(
+							"<option value='10041002'  selected = 'selected'>否</option>");
 		} else {
-			jQuery("#isJeep").append("<option value='0' selected = 'selected'>请选择</option>");
+			jQuery("#isJeep").append(
+					"<option value='0' selected = 'selected'>请选择</option>");
 			jQuery("#isJeep").append("<option value='10041001'>是</option>");
 			jQuery("#isJeep").append("<option value='10041002'>否</option>");
 		}
@@ -41,14 +51,19 @@ $(function() {
 	if (productCjd != null && productCjd != '') {
 		if (productCjd == '10041001') {
 			jQuery("#isCjd").append("<option value='0'>请选择</option>");
-			jQuery("#isCjd").append("<option value='10041001' selected = 'selected'>是</option>");
+			jQuery("#isCjd")
+					.append(
+							"<option value='10041001' selected = 'selected'>是</option>");
 			jQuery("#isCjd").append("<option value='10041002'>否</option>");
 		} else if (productCjd == '10041002') {
 			jQuery("#isCjd").append("<option value='0'>请选择</option>");
 			jQuery("#isCjd").append("<option value='10041001'>是</option>");
-			jQuery("#isCjd").append("<option value='10041002'  selected = 'selected'>否</option>");
+			jQuery("#isCjd")
+					.append(
+							"<option value='10041002'  selected = 'selected'>否</option>");
 		} else {
-			jQuery("#isCjd").append("<option value='0' selected = 'selected'>请选择</option>");
+			jQuery("#isCjd").append(
+					"<option value='0' selected = 'selected'>请选择</option>");
 			jQuery("#isCjd").append("<option value='10041001'>是</option>");
 			jQuery("#isCjd").append("<option value='10041002'>否</option>");
 		}
@@ -57,21 +72,25 @@ $(function() {
 	if (productDateRet != null && productDateRet != '') {
 		if (productDateRet == '12') {
 			jQuery("#productDate").append("<option value='0'>请选择</option>");
-			jQuery("#productDate").append("<option value='12' selected = 'selected'>12个月</option>");
+			jQuery("#productDate").append(
+					"<option value='12' selected = 'selected'>12个月</option>");
 			jQuery("#productDate").append("<option value='24'>24个月</option>");
 			jQuery("#productDate").append("<option value='36'>36个月</option>");
 		} else if (productDateRet == '24') {
 			jQuery("#productDate").append("<option value='0'>请选择</option>");
 			jQuery("#productDate").append("<option value='12'>12个月</option>");
-			jQuery("#productDate").append("<option value='24' selected = 'selected'>24个月</option>");
+			jQuery("#productDate").append(
+					"<option value='24' selected = 'selected'>24个月</option>");
 			jQuery("#productDate").append("<option value='36'>36个月</option>");
 		} else if (productDateRet == '36') {
 			jQuery("#productDate").append("<option value='0'>请选择</option>");
 			jQuery("#productDate").append("<option value='12'>12个月</option>");
 			jQuery("#productDate").append("<option value='24'>24个月</option>");
-			jQuery("#productDate").append("<option value='36' selected = 'selected'>36个月</option>");
+			jQuery("#productDate").append(
+					"<option value='36' selected = 'selected'>36个月</option>");
 		} else {
-			jQuery("#productDate").append("<option value='0' selected = 'selected'>请选择</option>");
+			jQuery("#productDate").append(
+					"<option value='0' selected = 'selected'>请选择</option>");
 			jQuery("#productDate").append("<option value='12'>12个月</option>");
 			jQuery("#productDate").append("<option value='24'>24个月</option>");
 			jQuery("#productDate").append("<option value='36'>36个月</option>");
@@ -92,10 +111,6 @@ $(function() {
 	var oTable = new TableInit();
 	oTable.Init();
 });
-
-
-
-
 
 /* 弹出的层级 */
 var index;
@@ -118,205 +133,196 @@ function addPart() {
 	parent.layer.close(index1)
 };
 
-var vm = new Vue(
-		{
-			el : '#editProduct',
-			data : {
-				q : {
-					productNo : null,
-					productName : null,
-					productCategory : null,
-					productProperty : null,
-					IS_VALID : null,
-					productDate : null,
-					releaseStatus : null,
-					productModel : null,
-					isCSales : null,
-					releaseStart : null,
-					releaseEnd : null,
-					salesStart : null,
-					salesEnd : null
+var vm = new Vue({
+	el : '#editProduct',
+	data : {
+		q : {
+			productNo : null,
+			productName : null,
+			productCategory : null,
+			productProperty : null,
+			IS_VALID : null,
+			productDate : null,
+			releaseStatus : null,
+			productModel : null,
+			isCSales : null,
+			releaseStart : null,
+			releaseEnd : null,
+			salesStart : null,
+			salesEnd : null
 
-				},
-				showOper : true,
-				title : null,
-				tiOpiExtendedDCS : {
-					PRODUCT_ID : null,
-					PRODUCT_NO : null,
-					PRODUCT_NAME : null,
-					PRODUCT_PROPERTY : null,
-					PRODUCT_CATEGORY : null,
-					DNP_PRICE : null,
-					MSRP_PRICE : null,
-					SINGLE_DISCOUNT : null,
-					IS_VALID : null,
-					CREATED_BY : null,
-					CREATED_AT : null,
-					UPDATED_BY : null,
-					UPDATED_AT : null,
-					IS_C_SALE : null,
-					PRODUCT_DATE : null,
-					PRODUCT_MODEL : null,
-					SALES_DATE : null,
-					RELEASE_DATE : null,
-					RELEASE_STATUS : null,
-					STOP_DATE : null,
-					PRODUCT_FAIT : null,
-					PRODUCT_JEEP : null,
-					PRODUCT_CJD : null,
-					PRODUCT_DESCRIBTION : null,
-					NON_TAX_COST_PRICE : null,
-					TAX_COST_PRICE : null,
-					TERMINAL_NON_SALES_PRICE : null,
-					TERMINAL_SALES_PRICE : null,
-					releaseStart : null,
-					releaseEnd : null,
-					salesStart : null,
-					salesEnd : null
-				}
-			},
-			methods : {
-				query : function() {
-					vm.reload();
-				},
-				mounted : function() {
-					console.log("vue start...");
-				},
-				addProductModel : function() {
-					var productId = $("#productId").val();
-					if (productId != '') {
-						layer.open({
-							title : '编辑产品',
-							type : 2,
-							area : [ '90%', '100%' ],
-							fixed : true, // 固定
-							maxmin : false,
-							content : ctx
-									+ '/sysProduct/ajax/searchModel?productId='
-									+ productId,
-							end : function() {
-								i = 0;
-							}
-						});
-						//var index1 = parent.layer.getFrameIndex(window.name);
-						//parent.layer.close(index1)
-					} else {
-						alert("请先去选择产品信息");
+		},
+		showOper : true,
+		title : null,
+		tiOpiExtendedDCS : {
+			PRODUCT_ID : null,
+			PRODUCT_NO : null,
+			PRODUCT_NAME : null,
+			PRODUCT_PROPERTY : null,
+			PRODUCT_CATEGORY : null,
+			DNP_PRICE : null,
+			MSRP_PRICE : null,
+			SINGLE_DISCOUNT : null,
+			IS_VALID : null,
+			CREATED_BY : null,
+			CREATED_AT : null,
+			UPDATED_BY : null,
+			UPDATED_AT : null,
+			IS_C_SALE : null,
+			PRODUCT_DATE : null,
+			PRODUCT_MODEL : null,
+			SALES_DATE : null,
+			RELEASE_DATE : null,
+			RELEASE_STATUS : null,
+			STOP_DATE : null,
+			PRODUCT_FAIT : null,
+			PRODUCT_JEEP : null,
+			PRODUCT_CJD : null,
+			PRODUCT_DESCRIBTION : null,
+			NON_TAX_COST_PRICE : null,
+			TAX_COST_PRICE : null,
+			TERMINAL_NON_SALES_PRICE : null,
+			TERMINAL_SALES_PRICE : null,
+			releaseStart : null,
+			releaseEnd : null,
+			salesStart : null,
+			salesEnd : null
+		}
+	},
+	methods : {
+		query : function() {
+			vm.reload();
+		},
+		mounted : function() {
+			console.log("vue start...");
+		},
+		addProductModel : function() {
+			var productId = $("#productId").val();
+			if (productId != '') {
+				layer.open({
+					title : '编辑产品',
+					type : 2,
+					area : [ '90%', '100%' ],
+					fixed : true, // 固定
+					maxmin : false,
+					content : ctx + '/sysProduct/ajax/searchModel?productId='
+							+ productId,
+					end : function() {
+						i = 0;
 					}
-				},
-				deleteModel : function() {
-					var productId = $("#productId").val();
-					if (productId != '') {
-						var selected = $('#table').bootstrapTable(
-								'getSelections');
-						console.log(selected.length);
-						for ( var item in selected) {
-							console.log(selected[item].PRODUCT_MODEL_ID)
-						}
-
-						$
-								.ajax({
-									type : "POST",
-									url : ctx
-											+ "/sysProduct/ajax/deleteProductModel",
-									contentType : "application/json",
-									dataType : "json",
-									data : JSON.stringify({
-										"listMap" : selected
-									}),
-									cache : false,
-									success : function(data) {
-										console.log("返回参数:" + data);
-										if (data == 0) {
-											vm.reload();
-											/*window.location.href = ctx
-													+ '/sysProduct/ajax/modelToProduct?productId='
-													+ $("#productId").val();*/
-										}
-									},
-									error : function(data) {
-										console.log(data);
-									}
-								});
-					} else {
-						alert("请先去选择一款产品!");
-						// layer.alert('请先去选择一款产品');
-					}
-				},
-				saveModel : function() {
-					var productId = $("#productId").val();
-					var salesStart = $("#salesStart").val();
-					var salesEnd = $("#salesEnd").val();
-					var isFiat = $("#isFiat").val();
-					var isJeep = $("#isJeep").val();
-					var isCjd = $("#isCjd").val();
-					if (productId != '') {
-						if (salesStart != '' && salesEnd != '') {
-							var param = {
-								PRODUCT_ID : $("#productId").val(),
-								PRODUCT_NO : $("#productNo").val(),
-								PRODUCT_NAME : $("#productName").val(),
-								PRODUCT_CATEGORY : $("#productCategory").val(),
-								PRODUCT_PROPERTY : $("#productProperty").val(),
-								PRODUCT_DATE : $("#productDate").val(),
-								DNP_PRICE : $("#dnpPrice").val(),
-								MSRP_PRICE : $("#msrpPrice").val(),
-								PRODUCT_FAIT : $("#isFiat").val(),
-								PRODUCT_JEEP : $("#isJeep").val(),
-								PRODUCT_CJD : $("#isCjd").val(),
-								PRODUCT_MODEL : $("#productModel").val(),
-								salesStart : salesStart,
-								salesEnd : salesEnd
-							}
-							$
-									.ajax({
-										type : "GET",
-										url : ctx
-												+ "/sysProduct/ajax/updateAndAddProduct",
-										contentType : "application/json",
-										dataType : "json",
-										data : param,
-										cache : false,
-										success : function(data) {
-											console.log("返回参数:" + data);
-											if (data == '0') {
-												alert("产品编辑成功");
-											}
-										},
-										error : function(data) {
-											console.log(data);
-										}
-									});
-						} else {
-							alert("产品销售时间不能为空");
-						}
-					} else {
-						alert("请先选择产品");
-					}
-
-				},
-				goBack : function() {
-					console.log("返回");
-					// console.log(parent.globe_index);
-					index = parent.layer.getFrameIndex(window.name);
-					parent.layer.close(index);
-				},
-				reload : function() {
-					vm.showOper = true;
-					$('#table').bootstrapTable('refresh');
-				}
+				});
+				// var index1 = parent.layer.getFrameIndex(window.name);
+				// parent.layer.close(index1)
+			} else {
+				alert("请先去选择产品信息");
 			}
-		});
+		},
+		deleteModel : function() {
+			var productId = $("#productId").val();
+			var selected = $('#table').bootstrapTable('getSelections');
+			if (productId != '' && selected.length > 0) {
+
+				console.log(selected.length);
+				for ( var item in selected) {
+					console.log(selected[item].PRODUCT_MODEL_ID)
+				}
+
+				$.ajax({
+					type : "POST",
+					url : ctx + "/sysProduct/ajax/deleteProductModel",
+					contentType : "application/json",
+					dataType : "json",
+					data : JSON.stringify({
+						"listMap" : selected
+					}),
+					cache : false,
+					success : function(data) {
+						console.log("返回参数:" + data);
+						if (data == 0) {
+							vm.reload();
+						}
+					},
+					error : function(data) {
+						console.log(data);
+					}
+				});
+			} else {
+				alert("请先去选择要删除的车型!");
+			}
+		},
+		saveModel : function() {
+			var productId = $("#productId").val();
+			var salesStart = $("#salesStart").val();
+			var salesEnd = $("#salesEnd").val();
+			var isFiat = $("#isFiat").val();
+			var isJeep = $("#isJeep").val();
+			var isCjd = $("#isCjd").val();
+			if (productId != '') {
+				if (salesStart != '' && salesEnd != '') {
+					var param = {
+						PRODUCT_ID : $("#productId").val(),
+						PRODUCT_NO : $("#productNo").val(),
+						PRODUCT_NAME : $("#productName").val(),
+						PRODUCT_CATEGORY : $("#productCategory").val(),
+						PRODUCT_PROPERTY : $("#productProperty").val(),
+						PRODUCT_DATE : $("#productDate").val(),
+						DNP_PRICE : $("#dnpPrice").val(),
+						MSRP_PRICE : $("#msrpPrice").val(),
+						PRODUCT_FAIT : $("#isFiat").val(),
+						PRODUCT_JEEP : $("#isJeep").val(),
+						PRODUCT_CJD : $("#isCjd").val(),
+						PRODUCT_MODEL : $("#productModel").val(),
+						salesStart : salesStart,
+						salesEnd : salesEnd
+					}
+					$.ajax({
+						type : "GET",
+						url : ctx + "/sysProduct/ajax/updateAndAddProduct",
+						contentType : "application/json",
+						dataType : "json",
+						data : param,
+						cache : false,
+						success : function(data) {
+							console.log("返回参数:" + data);
+							if (data == '0') {
+								alert("产品编辑成功");
+							}
+						},
+						error : function(data) {
+							console.log(data);
+						}
+					});
+				} else {
+					alert("产品销售时间不能为空");
+				}
+			} else {
+				alert("请先选择产品");
+			}
+
+		},
+		goBack : function() {
+			console.log("返回");
+			// console.log(parent.globe_index);
+			index = parent.layer.getFrameIndex(window.name);
+			parent.layer.close(index);
+		},
+		reload : function() {
+			vm.showOper = true;
+			$('#table').bootstrapTable('refresh');
+		}
+	}
+});
 
 function getProductDate() {
 	console.log("------------------------")
-	/*$("#productDate").empty();
-	$("#productDate").selectpicker('refresh');
-	jQuery("#productDate").append("<option value='0'>请选择</option>");
-	jQuery("#productDate").append("<option value='12'>12个月</option>");
-	jQuery("#productDate").append("<option value='24'>24个月</option>");
-	jQuery("#productDate").append("<option value='36'>36个月</option>");*/
-	
+	/*
+	 * $("#productDate").empty(); $("#productDate").selectpicker('refresh');
+	 * jQuery("#productDate").append("<option value='0'>请选择</option>");
+	 * jQuery("#productDate").append("<option value='12'>12个月</option>");
+	 * jQuery("#productDate").append("<option value='24'>24个月</option>");
+	 * jQuery("#productDate").append("<option value='36'>36个月</option>");
+	 */
+
 }
 
 var TableInit = function() {
@@ -445,11 +451,10 @@ var TableInit = function() {
 	return oTableInit;
 };
 
-
-function getModelList(values){
-	console.log("addModeltoProduct:"+values);
-	if(values == '0'){
+function getModelList(values) {
+	console.log("addModeltoProduct:" + values);
+	if (values == '0') {
 		vm.reload();
 	}
-	
+
 }
